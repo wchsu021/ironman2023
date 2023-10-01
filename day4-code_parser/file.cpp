71252163b3ef161e9784103002f30b6bb3f1bcf8
Wei-Chen Hsu <ggsg1085@gmail.com>
2023-10-01 18:31:04 +0800

    update day4

---
+++ day4-code_parser/file.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main() {
+    int age = 100;  // Input user's age
+    char* name = "BarleyTea";   // Input user's name
+    printf("Hi, my name is %s. I am %d years old.\n", name, age);   // print user info
+}
